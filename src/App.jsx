d25f0ad66dbb8f6de7b52ddece6a000aae1a24f6
AlwaysMudgin <193750882+AlwaysMudgin@users.noreply.github.com>
2025-11-26 15:06:38 -0500

    Added attribution, fixed minimum widths on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,22 @@ function App() {
     <>
       <LogoHeader />
       <MainCard />
+      <footer>
+        <p>
+          Challenge by&nbsp;
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          .
+        </p>
+        <p>
+          Coded by&nbsp;
+          <a href="https://github.com/AlwaysMudgin/FM-tip-calculator">
+            AlwaysMudgin
+          </a>
+          .
+        </p>
+      </footer>
     </>
   );
 }
